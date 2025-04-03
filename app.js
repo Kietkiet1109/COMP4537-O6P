70d1cfc9d1489d8a4b935bb9bc67f9867bf348fb
Cyconi <cyconiexo@gmail.com>
2025-04-03 12:11:34 -0700

    did you pray today

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use((req, res, next) =>
 {
     res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
     res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+    res.setHeader("Content-Security-Policy", "default-src 'self'; font-src 'self' https://comp4537-project-5ddxc.ondigitalocean.app;");
     next();
 });
 
