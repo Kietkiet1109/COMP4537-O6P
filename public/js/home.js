6f7a94a5acdc244d574aeacd4bc18b1004d2ed26
BrandonRada <brandonn.rada@gmail.com>
2025-04-03 01:41:57 -0700

    messed up api call

--- public/js/home.js
+++ public/js/home.js
@@ -70,7 +70,7 @@ router.get('/admin', async (req, res) => {
         console.log("Checking admin status...");
 
         // Call the adminSearch method to get the necessary data
-        const searchData = await makeApiRequest('/currentUser', { method: 'GET', options: {req} });
+        const searchData = await makeApiRequest('/adminSearch', { method: 'GET', options: {req} });
 
         // Check if the response is valid and contains the necessary data
         if (searchData.success) {
