93faca6ed7328dfc3eac8f54dc15dbd8ad8c7a17
Cyconi <cyconiexo@gmail.com>
2025-04-03 12:07:07 -0700

    sdfsda

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.use((req, res, next) =>
 {
     res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
     res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-    res.setHeader("Content-Security-Policy", "font-src 'self' https://comp4537-project-5ddxc.ondigitalocean.app; default-src 'none';");
     next();
 });
 
